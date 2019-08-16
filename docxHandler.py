b850e4253a20c8f747b8e89c3e2c48f51b83c17c
plus <maxcrazy@yeah.net>
2019-08-16 16:32:29 +0800

    修改图片注释的样式
    	修改：     docxHandler.py

--- docxHandler.py
+++ docxHandler.py
@@ -1,6 +1,8 @@
 import sys
 from docx import Document
 from docx.shared import RGBColor
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 sample = '''
 sample:
@@ -17,6 +19,9 @@ doc = Document("{}/README.docx".format(target))
 style = doc.styles["Source Code"]
 style.font.shadow = True
 style.font.color.rgb = RGBColor(0xFF, 0x00, 0xFF)
+style = doc.styles["Image Caption"]
+style.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+style.font.color.rgb = RGBColor(0xAA, 0xAA, 0xAA)
 
 for para in doc.paragraphs:
     para.paragraph_format.line_spacing = 1.5
